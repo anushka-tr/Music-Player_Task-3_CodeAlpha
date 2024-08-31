dcf154c8a8b3a80a7f175decfbc2f274df123add
Anushka Trivedi <anushkatrivedi93057@gmail.com>
2024-08-31 20:31:16 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,111 @@
+"use strict";
+const imgEl = document.getElementById("bg_img");
+const imgCoverEl = document.getElementById("cover");
+const musicTitleEl = document.getElementById("music_title");
+const musicArtistEl = document.getElementById("musric_artist");
+const playerProgressEl = document.getElementById("player_progress");
+const progressEl = document.getElementById("progress");
+const currentTimeEl = document.getElementById("current_time");
+const durationEl = document.getElementById("duration");
+const prevBtnEl = document.getElementById("prev");
+const playvBtnEl = document.getElementById("play");
+const nextvBtnEl = document.getElementById("next");
+const songs = [
+  {
+    path: "Music/I-Like-Me-Better-When-Im-With-You_320(PagalWorld).mp3",
+    displayName: "I Like Me Better",
+    cover: "Images/I-Like-Me-Better.jpg",
+    artist: "Lauv",
+  },
+  {
+    path: "Music/I-Love-You-For-Infinity(PagalWorld).mp3",
+    displayName: "Infinity",
+    cover: "Images/Infinity.jpg",
+    artist: "Jaymes Young",
+  },
+  {
+    path: "Music/Perfect Ed Sheeran-(PagalSongs.Com.IN).mp3",
+    displayName: "Perfect",
+    cover: "Images/Perfect.webp",
+    artist: "Ed Sheeran",
+  },
+  {
+    path: "Music/Stay(PagalNew.Com.Se).mp3",
+    displayName: "Stay",
+    cover: "Images/Stay.jpg",
+    artist: "Justin Bieber",
+  },
+];
+const music = new Audio();
+let musicIndex = 0;
+let isPlaying = false;
+//================== Play Song  True or False====================
+function togglePlay() {
+  if (isPlaying) {
+    pauseMusic();
+  } else {
+    playMusic();
+  }
+}
+//================== Play Music====================
+function playMusic() {
+  isPlaying = true;
+  playvBtnEl.classList.replace("fa-play", "fa-pause");
+  playvBtnEl.setAttribute("title", "pause");
+  music.play();
+}
+//================== Pause Music====================
+function pauseMusic() {
+  isPlaying = false;
+  playvBtnEl.classList.replace("fa-pause", "fa-play");
+  playvBtnEl.setAttribute("pause", "title");
+  music.pause();
+}
+//================== Load Songs ====================
+function loadMusic(songs) {
+  music.src = songs.path;
+  musicTitleEl.textContent = songs.displayName;
+  musicArtistEl.textContent = songs.artist;
+  imgCoverEl.src = songs.cover;
+  imgEl.src = songs.cover;
+}
+//================== Change Music ====================
+function changeMusic(direction) {
+  musicIndex = musicIndex + direction + (songs.length % songs.length);
+  loadMusic(songs[musicIndex]);
+  playMusic();
+}
+//================== Set Progress ====================
+function setProgressBar(e) {
+  const width = playerProgressEl.clientWidth;
+  const xValue = e.offsetX;
+  music.currentTime = (xValue / width) * music.duration;
+}
+//================== Set Progress ====================
+function updateProgressBar() {
+  const { duration, currentTime } = music;
+  const ProgressPercent = (currentTime / duration) * 100;
+  progressEl.style.width = `${ProgressPercent}%`;
+  const formattime = (timeRanges) =>
+    String(Math.floor(timeRanges)).padStart(2, "0");
+  durationEl.textContent = `${formattime(duration / 60)} : ${formattime(
+    duration % 60,
+  )}`;
+  currentTimeEl.textContent = `${formattime(currentTime / 60)} : ${formattime(
+    currentTime % 60,
+  )}`;
+}
+//================= Btn Events========================
+const btnEvents = () => {
+  playvBtnEl.addEventListener("click", togglePlay);
+  nextvBtnEl.addEventListener("click", () => changeMusic(1));
+  prevBtnEl.addEventListener("click", () => changeMusic(-1));
+  //========= Progressbar===========================
+  music.addEventListener("ended", () => changeMusic(1));
+  music.addEventListener("timeupdate", updateProgressBar);
+  playerProgressEl.addEventListener("click", setProgressBar);
+};
+//================= Btn Events========================
+document.addEventListener("DOMContentLoaded", btnEvents);
+//============ Calling Load Music
+loadMusic(songs[musicIndex]);
